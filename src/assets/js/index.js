065c4a7e2fb35cd46ab99c171e34c9db28ebee05
ajrodasr <er.rodas91@gmail.com>
2021-04-13 17:58:09 +0200

    Menu responsive

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+const element = document.getElementById('res-menu');
+const menuItems = document.getElementById('res-menu-items');
+
+element.addEventListener('click', (event) => {
+  if (menuItems.classList.contains('hidden')) {
+    menuItems.classList.remove('hidden');
+    console.log('Mostrando...');
+  } else {
+    menuItems.classList.add('hidden');
+    console.log('Ocultando...');
+  }
+});
