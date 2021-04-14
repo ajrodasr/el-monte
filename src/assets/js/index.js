8d9bc0947c5e220f2f2bf7ef457e29cc495d8050
ajrodasr <er.rodas91@gmail.com>
2021-04-14 20:01:06 +0200

    Añadido Inicio al menu responsive. Creadas paginas de categorias y detalles.

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,12 +1,16 @@
 const element = document.getElementById('res-menu');
 const menuItems = document.getElementById('res-menu-items');
+const menuIcon = document.getElementById('open-menu');
+const closeMenuIcon = document.getElementById('close-menu');
 
 element.addEventListener('click', (event) => {
   if (menuItems.classList.contains('hidden')) {
     menuItems.classList.remove('hidden');
-    console.log('Mostrando...');
+    menuIcon.classList.add('hidden');
+    closeMenuIcon.classList.remove('hidden');
   } else {
     menuItems.classList.add('hidden');
-    console.log('Ocultando...');
+    menuIcon.classList.remove('hidden');
+    closeMenuIcon.classList.add('hidden');
   }
 });
